87a80bea1e2b733bced1eb2c02fd2bced29763b0
Mack <hypermail@yandex.ru>
2024-04-23 11:41:13 +0300

    HTMX

---
+++ books/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm, CharField, TextInput
+from .models import Book
+
+
+class BookCreateForm(ModelForm):
+    title = CharField(required=False, widget=TextInput(attrs={"class": "clrtxt", "placeholder": "Title"}))
+    author = CharField(required=False, widget=TextInput(attrs={"class": "clrtxt", "placeholder": "Author"}))
+    price = CharField(required=False, widget=TextInput(attrs={"class": "clrtxt", "placeholder": "Price"}))
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'price']
+
+
+class BookEditForm(BookCreateForm):
+    title = CharField(required=False, widget=TextInput())
+    author = CharField(required=False, widget=TextInput())
+    price = CharField(required=False, widget=TextInput())
